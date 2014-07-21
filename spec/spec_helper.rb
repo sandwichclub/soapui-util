e2e349876088b0464d4c5b9442ab23f91eb862d0
cyrus <cyrus>
2014-07-20 23:39:12 -0700

    trying to debug travis ci

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ begin
   puts "LOG: which java returned: #{java_path}"
   java_home = %x(echo $JAVA_HOME)
   puts "LOG: JAVA_HOME: '#{java_home}'"
-  ENV['JAVA_HOME'] = java_home
+  #ENV['JAVA_HOME'] = java_home
 rescue
   puts "WARN: Problem setting JAVA_HOME to: '#{java_home}'"
 end
